379e2619f94205d2bb81d9bde1167e76ab4f6e6b
forgacs.attila@gmail.com <forgacs.attila@gmail.com>
2013-07-19 21:37:19 +0200

    update

--- 7/circles_equations_2.py
+++ 7/circles_equations_2.py
@@ -6,6 +6,7 @@
 from sympy import *
 import numpy as np
 import scipy as sp
+import time
 %load_ext sympyprinting
 #%load_ext sympy.interactive.ipythonprintingthonprinting
 #%pylab inline
@@ -20,16 +21,28 @@ init_printing()
 
 # <codecell>
 
-#Profile P6:     width: 6     height:2,5     ringwidth/inner circumfence 56     volume:0,758 cm³
+'''
+Volume:
+Profile P2:     width: 4     height:2        ringwidth/inner circumfence 56     volume:0,396 cm³
+Profile P3:     width: 5     height:1,5     ringwidth/inner circumfence 56     volume:0,336 cm³
+Profile P4:     width: 7     height:3        ringwidth/inner circumfence 56     volume:1,155 cm³
+Profile P5:     width: 3     height:1        ringwidth/inner circumfence 56     volume:0,144 cm³
+Profile P6:     width: 6     height:2,5     ringwidth/inner circumfence 56     volume:0,758 cm³
+'''
 
 #R51, R52
-W,H  = 5. , 1.5     
+W=3.
+H=1.
 CF = 56
 RI = (CF / (2.*pi) )
-RI = round(RI,4)
+RI = round(RI,6)
 
-bbc_R  = 6.38
-btc_R  = 6.38
+#R60
+bbc_R  = 5.2
+#R61
+btc_R  = 25.6
+
+# <codecell>
 
 # calculated
 bbc_CX = W/2.
@@ -45,6 +58,8 @@ rsc_CX = Symbol('rsc_CX')
 rsc_CY = Symbol('rsc_CY')
 W,H,RI
 bbc_CX,bbc_CY,btc_CX,btc_CY
+RI+1.5/2
+print 'RI=%s RI+1.5=%s',RI, RI+1.5
 
 # <codecell>
 
@@ -96,10 +111,6 @@ variables
 
 # <codecell>
 
-#from sympy import ask, Q
-#global_assumptions.add(Q.positive(lsc_R))
-import time
-
 t1=time.time()
 solutions = solve(   eq_system , *variables , dict=True )
 t2=time.time()
@@ -117,6 +128,8 @@ if RI+H >s[lsc_CY]> RI and
    RI+H >s[rsc_CY]> RI
 ]
 len(filtered_solutions)
+for s in filtered_solutions[0]:
+    print s,'\t:',filtered_solutions[0][s]
 
 # <codecell>
 
@@ -159,21 +172,49 @@ plt.show()
 
 # <codecell>
 
-P(S[lsc_CX],S[lsc_CY])
-
-# <codecell>
-
-volume = __builtin__.sum ([
+top = __builtin__.sum ([
    volume_integrate_arc_top(P(S[lsc_CX],S[lsc_CY]), S[lsc_R], 0., S[Symbol('p1_X')]),
    volume_integrate_arc_top(P(btc_CX,btc_CY), btc_R, S[Symbol('p1_X')], S[Symbol('p2_X')]),
    volume_integrate_arc_top(P(S[rsc_CX],S[rsc_CY]), S[rsc_R], S[Symbol('p2_X')], W),
-]) - __builtin__.sum ([
+]) 
+bottom = __builtin__.sum ([
    volume_integrate_arc_bottom(P(S[lsc_CX],S[lsc_CY]), S[lsc_R], 0. , S[Symbol('p3_X')]),
-   volume_integrate_arc_bottom(P(btc_CX,btc_CY), btc_R, S[Symbol('p3_X')], S[Symbol('p4_X')]),
+   volume_integrate_arc_bottom(P(bbc_CX,bbc_CY), bbc_R, S[Symbol('p3_X')], S[Symbol('p4_X')]),
    volume_integrate_arc_bottom(P(S[rsc_CX],S[rsc_CY]), S[rsc_R], S[Symbol('p4_X')], W),             
-])                       
+]) 
 
-volume
+print 'top_vol=',top
+print 'bottom_vol=',bottom
+volume=top-bottom
+print 'vol=',volume
+
+# <codecell>
+
+[
+   volume_integrate_arc_top(P(S[lsc_CX],S[lsc_CY]), S[lsc_R], 0., S[Symbol('p1_X')]),
+   volume_integrate_arc_top(P(btc_CX,btc_CY), btc_R, S[Symbol('p1_X')], S[Symbol('p2_X')]),
+   volume_integrate_arc_top(P(S[rsc_CX],S[rsc_CY]), S[rsc_R], S[Symbol('p2_X')], W),
+]
+
+# <codecell>
+
+[
+   volume_integrate_arc_bottom(P(S[lsc_CX],S[lsc_CY]), S[lsc_R], 0. , S[Symbol('p3_X')]),
+   volume_integrate_arc_bottom(P(bbc_CX,bbc_CY), bbc_R, S[Symbol('p3_X')], S[Symbol('p4_X')]),
+   volume_integrate_arc_bottom(P(S[rsc_CX],S[rsc_CY]), S[rsc_R], S[Symbol('p4_X')], W),             
+]
+
+# <codecell>
+
+volume_integrate_arc_top(P(S[lsc_CX],S[lsc_CY]), S[lsc_R], 0., S[Symbol('p1_X')])
+
+# <codecell>
+
+volume_integrate_arc_top(P(btc_CX,btc_CY), btc_R, S[Symbol('p1_X')], S[Symbol('p2_X')])
+
+# <codecell>
+
+volume_integrate_arc_top(P(S[rsc_CX],S[rsc_CY]), S[rsc_R], S[Symbol('p2_X')], W)
 
 # <rawcell>
 
@@ -188,4 +229,13 @@ volume
 # Profile P4:     width: 7     height:3        ringwidth/inner circumfence 56     volume:1,155 cm³
 # Profile P5:     width: 3     height:1        ringwidth/inner circumfence 56     volume:0,144 cm³
 # Profile P6:     width: 6     height:2,5     ringwidth/inner circumfence 56     volume:0,758 cm³
+
+# <codecell>
+
+ours = 155.559839915
+theirs = 144
+diff = ours - theirs
+deviation = diff / min(theirs,ours)
+print 'difference=',diff
+print 'dev =',deviation*100,' %'
 
